cea77cea8a60612c7c894c813f01a00b43e34e18
Stereo2k <stereo2k2013@ya.ru>
2020-12-15 16:40:46 +0300

    first commit2

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.header__menu-burger').click(function(){
+        $('.header__menu-burger').toggleClass('open-menu');
+        $('.header__menu').toggleClass('open-menu');
+    });
+});
+
+
+
+$('.slick__photo').slick({
+arrows:false,
+dots:true
+});
